ac533e94f25f77447bdeb2008843dccb5893b98b
Harry <harry_xie@live.com>
2018-02-01 01:27:13 -0500

    tidy

--- src/dataset.py
+++ src/dataset.py
@@ -1,3 +1,4 @@
+import torch
 from torch.utils.data import Dataset
 from torchvision import transforms, utils
 from PIL import Image
@@ -6,20 +7,21 @@ import pickle
 import os
 from datetime import datetime
 
+transform = transforms.Compose([transforms.ToTensor()])
+
 class KeypointsDataset(Dataset):
-    def __init__(self, img_folder, labels, num_classes, img_height, img_width, radius):
+    def __init__(self, img_folder, labels, num_classes, img_height, img_width, radius, transform):
         self.num_classes = num_classes
         self.img_height = img_height
         self.img_width = img_width
-        self.radius = radius
-        
+        self.radius = radius         
+        self.transform = transform
+
         self.imgs = []
         self.labels = labels
 
         for i in range(len(self.labels)):
             self.imgs.append(os.path.join(img_folder, str(i)+'.jpg'))
-            
-        self.transform = transforms.Compose([transforms.ToTensor()])
         
         self.map_value = np.array([[np.linalg.norm([self.img_width - _x, self.img_height - _y]) 
                           for _x in range(img_width * 2)] for _y in range(img_height * 2)])
@@ -30,35 +32,46 @@ class KeypointsDataset(Dataset):
                                          for _y in range(self.img_height * 2)])
         
     def __getitem__(self, index):  
-        
+       
+        starttime = datetime.now() 
         img = self.transform(Image.open(self.imgs[index]))
         labels = self.labels[index]
-        
+
+        visible = np.zeros(self.num_classes)
+        keypoints = np.zeros((self.num_classes, 2))      
+     
         maps = np.zeros((self.num_classes, self.img_height, self.img_width), dtype='float32')
         offsets_x = np.zeros((self.num_classes, self.img_height, self.img_width), dtype='float32')
         offsets_y = np.zeros((self.num_classes, self.img_height, self.img_width), dtype='float32')
-        # starttime = datetime.now()
+        
         for i in range(0, self.num_classes * 3, 3):
             x = labels[i]
             y = labels[i + 1]
             
             _i = i // 3
+
+            if labels[i + 2] > 0:
+                visible[_i] = 1
+            else:
+                visible[_i] = 0
+            
+            keypoints[_i][0] = x
+            keypoints[_i][1] = y
+
             if x == 0 and y == 0:
                 maps[_i] = np.zeros((self.img_height, self.img_width))
                 continue
             if self.img_height - y < 0 or self.img_width - x < 0:
-                continue             
+                continue          
             maps[_i] = self.map_value[self.img_height - y : self.img_height * 2 - y, 
-                                      self.img_width  - x : self.img_width * 2  - x]
-            
+                                      self.img_width  - x : self.img_width * 2  - x]       
             maps[_i][maps[_i] <= self.radius] = 1
             maps[_i][maps[_i] >  self.radius] = 0
             offsets_x[_i] = self.offsets_x_value[self.img_height - y : self.img_height * 2 - y, 
                                                  self.img_width  - x : self.img_width * 2  - x]
             offsets_y[_i] = self.offsets_y_value[self.img_height - y : self.img_height * 2 - y, 
                                                  self.img_width  - x : self.img_width * 2  - x]      
-
-        return img, (maps, offsets_x, offsets_y), labels
+        return img, (maps, offsets_x, offsets_y), (visible, keypoints)
     
     def __len__(self):
         return len(self.labels)
--- train.py
+++ train.py
@@ -2,17 +2,78 @@ import pickle
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from config import NUM_CLASSES, IMG_HEIGHT, IMG_WIDTH, RADIUS, epochs, batch_size
 from src.model import Keypoints
-from src.dataset import KeypointsDataset
+from src.dataset import KeypointsDataset, transform
+
+def custom_loss(predictions_maps, maps, predictions_offsets_x, offsets_x, predictions_offsets_y, offsets_y):
+    
+    loss_h = bceLoss(predictions_maps, maps)
+
+    distance_x = predictions_offsets_x[maps==1] - offsets_x[maps==1]
+    distance_y = predictions_offsets_y[maps==1] - offsets_y[maps==1]
+    distances = torch.sqrt(distance_x * distance_x + distance_y * distance_y)
+    zero_distances = Variable(
+        torch.zeros(distance_x.shape).cuda() if use_cuda else torch.zeros(distance_x.shape))
+    loss_o = smoothL1Loss(distances, zero_distances)
+    loss = 4 * loss_h + loss_o
+    return loss
+
+def forward(sample_batched, model):
+    X = sample_batched[0]
+    maps, offsets_x, offsets_y = sample_batched[1]
+
+    maps = Variable(maps.cuda() if use_cuda else maps)
+    offsets_x = Variable(offsets_x.cuda() if use_cuda else offsets_x)
+    offsets_y = Variable(offsets_y.cuda() if use_cuda else offsets_y)
+
+    # forward
+    X = Variable(X.cuda() if use_cuda else X)
+    (predictions_maps, predictions_offsets_x, predictions_offsets_y), pred = model.forward(X)
+    
+    return custom_loss(predictions_maps, maps, predictions_offsets_x, offsets_x, predictions_offsets_y, offsets_y)
+
+def fit(train_data, test_data, model, loss_function, epochs, checkpoint_path = ''):
+    for epoch in range(epochs):
+        # training 
+        train_loss = 0.0
+        for i_batch, sample_batched in enumerate(train_data):
+            optimizer.zero_grad()
+            
+            loss = forward(sample_batched, model)
+            
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.data[0]
+
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i_batch + 1, loss.data[0]), end='')
+            print('\r', end='')
+        print('train loss:', train_loss / i_batch)
+        
+        test_loss = 0.0
+        for i_batch, sample_batched in enumerate(test_data):
+            loss = forward(sample_batched, model)
+            test_loss += loss.data[0]
+        print('test loss:', test_loss / i_batch)
+        
+        torch.save(keypoints.state_dict(), checkpoint_path + 'model_2_1_' + str(epoch)+'.pth')
 
 # dataset
 with open('../data/annotation/annotation_train_cropped_humans.pkl', 'rb') as f:
-    labels = pickle.load(f)
-dataset = KeypointsDataset('../data/train_cropped_humans/', labels, NUM_CLASSES, IMG_HEIGHT, IMG_WIDTH, RADIUS)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    train_labels = pickle.load(f)
+train_dataset = KeypointsDataset('../data/train_cropped_humans/',
+                           train_labels, NUM_CLASSES, IMG_HEIGHT, IMG_WIDTH, RADIUS, transform=transform)
+train_data = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=32)
+
+with open('../data/annotation/annotation_test_cropped_humans.pkl', 'rb') as f:
+    test_labels = pickle.load(f)
+test_dataset = KeypointsDataset('../data/test_cropped_humans/',
+                           test_labels, NUM_CLASSES, IMG_HEIGHT, IMG_WIDTH, RADIUS, transform=transform)
+test_data = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=32)
 
 use_cuda = torch.cuda.is_available()
 # use_cuda = False
@@ -29,8 +90,32 @@ keypoints = keypoints.cuda() if use_cuda else keypoints
 # optimizer
 optimizer = optim.Adam(keypoints.parameters(), lr=0.0001)
 
+fit(train_data, test_data, keypoints, custom_loss, epochs=200, checkpoint_path='../checkpoints/')
+'''
 for epoch in range(epochs):
-    running_loss = 0.0
+    r dataset
+with open('../data/annotation/annotation_train_cropped_humans.pkl', 'rb') as f:
+    labels = pickle.load(f)
+dataset = KeypointsDataset('../data/train_cropped_humans/', 
+                           labels, NUM_CLASSES, IMG_HEIGHT, IMG_WIDTH, RADIUS, transform=transform)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=32)
+
+use_cuda = torch.cuda.is_available()
+# use_cuda = False
+Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+if use_cuda:
+    torch.cuda.set_device(2)
+
+# loss
+smoothL1Loss = nn.SmoothL1Loss()
+bceLoss = nn.BCELoss()
+# model
+keypoints = Keypoints(NUM_CLASSES)
+keypoints = keypoints.cuda() if use_cuda else keypoints
+# optimizer
+optimizer = optim.Adam(keypoints.parameters(), lr=0.0001)
+
+unning_loss = 0.0
     for i_batch, sample_batched in enumerate(dataloader):
         
         optimizer.zero_grad()
@@ -67,4 +152,4 @@ for epoch in range(epochs):
     print(running_loss / i_batch)
     
     torch.save(keypoints.state_dict(), '../checkpoints/model_1_18_'+str(epoch)+'.pth')
-
+'''
